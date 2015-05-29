5a7d8dc40c95638d66784fd71c497810826d036c
jutt <jutt@iupui.edu>
2015-05-29 11:40:52 -0400

    Uppercase and corrective property declarations

--- tools.js
+++ tools.js
@@ -1,15 +1,4 @@
- 
-/* ====================================================== */
-/*				Custom JavaScript tools					  */
-/*				-----------------------					  */
-/*				1.) A link generator					  */
-/*				2.) Breadcrumb generator				  */
-/* ====================================================== */
-
-/* ====================================================== */
-/* 1.) Creates a link with two inputs (location, text)    */
-/* ====================================================== */ 
- var aString = {
+var aString = {
     location: '',
     text: '', 
     createString: function(location, text)
@@ -24,15 +13,11 @@
         return string;
     }
   };
-  
-/* ====================================================== */
-/* 2.) Creates a breadcrumb string on pages. Uses h2 or   */
-/*		h3 if nothing is set for h2						  */
-/* ====================================================== */
+
   var breadcrumb = {
     hostname: window.location.host,
     urlName: window.location + '',
-    breadString: breadString = "<a href='"+hostName+"'>Home</a> > ",
+    breadString: aString.createString(this.hostname, 'Home') + " > ",
     parameters: '#main-content', //can change depending on which page you are on.
     pageTitle: '',
     h3Title : '', 
@@ -40,16 +25,16 @@
     parPrint: function()
     {
         this.pageTitle = $(this.parameters+' h2').first().text();
-        this.h3title = $(this.parameters+' h3').first().text();
+        this.h3Title = $(this.parameters+' h3').first().text();
 
         return this.pageTitle;
-        return this.h3title; 
+        return this.h3Title; 
     },
     removeLast: function()
     {
         for ( i = 0; i<=this.urlArray.length -3; i++ )
         {
-            this.breadString = this.breadString + this.urlArray[i] + " > "; 
+            this.breadString = this.breadString + this.urlArray[i].toUpperCase() + ' > '; 
         }
         this.breadString = this.breadString + this.urlArray[i] + " > ";
     },
@@ -58,23 +43,24 @@
     {
         if ( this.pageTitle == '' )
         {
-            this.pageTitle = h3Title;
+            this.pageTitle = this.h3Title;
         }
+        return this.pageTitle;
     },
      urlArray : function()
     {
         this.urlArray = this.urlName.split('/');
         this.urlArray = this.urlArray.splice(4);
-        return urlArray; 
-        
+        return this.urlArray;   
     },
     printString: function()
     {
         this.parPrint();
-         this.urlArray();
+        this.urlArray();
         this.removeLast();
         this.newtitle(); 
-        this.breadString = this.breadString + this.parPrint();
+        this.breadString = this.breadString + this.newtitle().toLowerCase();
         return this.breadString;
+
     }
   };
